a276f34a048eea5b57367b6328f5c7001300454b
Natasha-Stojanova <natashastojanova6@gmail.com>
2020-06-22 14:34:18 +0200

    Project init
    Signed-off-by: NatashaStojanova <natashastojanova6@gmail.com>

---
+++ backend/src/main/java/mk/ukim/finki/natashastojanova/hospital_management/HospitalManagementApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.natashastojanova.hospital_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/mk/ukim/finki/natashastojanova/hospital_management/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.natashastojanova.hospital_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
